2aea5b6fe385e90a64856b3b6a58f688b15966fb
cesar <cquesr175@gmail.com>
2022-11-21 16:33:52 -0500

    ejercicios de prueba

---
+++ Ejercicio3cqr.py
@@ -0,0 +1,7 @@
+peso = input("Ingrese su peso en Kg: ")
+talla = input("Igrese su talla en metros: ")
+
+pesof = float(peso)
+tallaf = float(talla)
+ICM = pesof/tallaf
+print("El IMC es: ", round(ICM,2))
---
+++ ejercicio4cqr.py
@@ -0,0 +1,2 @@
+for h in range (100,0,-1):
+    print(h)
